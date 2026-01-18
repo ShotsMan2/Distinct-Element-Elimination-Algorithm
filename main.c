283056b969f56c2e0720f3fd460e7dd373a7f1c9
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 23:00:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,48 +3,53 @@
 
 int main()
 {
-     int sayilar[]= { 1, 6, 5, 2, 1, 4, 2 };
-    int i,sayac,sayi,fsayac,varmi;
+    int sayilar[] = {1, 6, 5, 2, 1, 4, 2};
+    int i, sayac, sayi, fsayac, varmi;
 
+    for (;;)
+    {
 
+        sayac = 0;
 
-    for(;;){
-
-        sayac=0;
-
-        for(i=0;i<=6;i++){
-            if(sayilar[i]==-1){
+        for (i = 0; i <= 6; i++)
+        {
+            if (sayilar[i] == -1)
+            {
                 sayac++;
             }
         }
-        if(sayac==7){
+        if (sayac == 7)
+        {
             break;
         }
 
         printf("sayi girin ");
-        scanf("%d",&sayi);
+        scanf("%d", &sayi);
 
-        varmi=0;
+        varmi = 0;
 
-        for(i=0;i<=6;i++){
-            if(sayilar[i]==sayi){
-                varmi=1;
+        for (i = 0; i <= 6; i++)
+        {
+            if (sayilar[i] == sayi)
+            {
+                varmi = 1;
                 break;
             }
         }
 
-        if(varmi==1){
+        if (varmi == 1)
+        {
             fsayac++;
         }
 
-        for(i=0;i<=6;i++){
-            if(sayilar[i]==sayi){
-                sayilar[i]=-1;
-
-
+        for (i = 0; i <= 6; i++)
+        {
+            if (sayilar[i] == sayi)
+            {
+                sayilar[i] = -1;
             }
         }
     }
-    printf("%d tane farklı sayı bulunmaktadır.",fsayac);
+    printf("%d tane farklı sayı bulunmaktadır.", fsayac);
     return 0;
 }
